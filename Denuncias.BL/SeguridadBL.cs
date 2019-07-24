91a8f54ce3732189e9af436517d88a1b965c2fea
Ycaballero12 <yarecaballe1998@gmail.com>
2019-07-24 16:17:27 -0600

    Creando clase de autorizacion de usuarios al sistema

---
+++ Denuncias.BL/SeguridadBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denuncias.BL
+{
+    public class SeguridadBL
+    {
+        public bool Autorizar(string usuario, string contraseña)
+        {
+            if (usuario == "admin" && contraseña == "123")
+            {
+                return  true;
+            }
+            else
+            {
+                if (usuario == "user" && contraseña == "456")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- ManagerRequest/FormLogin.cs
+++ ManagerRequest/FormLogin.cs
@@ -1,49 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Denuncias.BL;
+using System;
 using System.Windows.Forms;
+
+
 
 namespace ManagerRequest
 {
     public partial class FormLogin : Form
     {
+        SeguridadBL _seguridad;
+
         public FormLogin()
         {
             InitializeComponent();
+            _seguridad = new SeguridadBL();
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-
+            
             string usuario = textBox1.Text;
             string contraseña = textBox2.Text;
 
-            if (usuario == "admin" && contraseña == "123")
+          bool resultado = _seguridad.Autorizar(usuario, contraseña);
+
+            if (resultado == true)
             {
                 this.Hide();
                 var form = new Form1();
                 form.Show();
-
             }
-            else
-            {
-                if (usuario == "user" && contraseña == "456")
-                {
-                    this.Hide();
-                    var form = new Form1();
-                    form.Show();
-                }
+            
                 else{
                     MessageBox.Show("usuario o contraseña incorrecta");
                 }
-                
-            }
-
+               
         }
 
         private void Button2_Click(object sender, EventArgs e)
