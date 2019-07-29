3c79e3ccfa1d0096a2e35835fbb86b29477f1a09
honduras <honduras@HONDURAS-PC>
2019-07-28 20:34:30 -0600

    Creacion de Clases BL

---
+++ Denuncias.BL/AsuntosBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denuncias.BL
+{
+    public class AsuntosBL
+    {
+        public AsuntosBL()
+        {
+
+
+        }
+
+        public class Asunto
+        {
+            public int Id { get; set; }
+            public string Descripcion { get; set; }
+            public string Operador { get; set; } //Quien resuelve el asunto
+            public bool activo { get; set; }
+            public string multiple { get; set; }
+
+        }
+    }
+}
---
+++ Denuncias.BL/DocumentosBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denuncias.BL
+{
+   public  class DocumentosBL
+    {
+        public DocumentosBL()
+        {
+
+        }
+
+        public class Documento
+        {
+            public int Id { get; set; }
+            public string Descripcion { get; set; }
+            public string  Usuariocreador { get; set; }
+            public string Compania { get; set; }
+            public bool Activo { get; set; }
+        }
+
+
+
+
+    }
+}
