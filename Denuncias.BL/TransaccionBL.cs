bc00695920f7a5a4bc7faf4b013749194c50183f
Josua Cruz <cruza@JOSUAPC>
2019-09-02 21:42:48 -0600

    Creaccion de clase transaccion

---
+++ Denuncias.BL/TransaccionBL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denuncias.BL
+{
+    public class TransaccionBL
+    {
+
+        Contexto _contexto;
+
+        public BindingList<Transaccion> ListaTransaccion { get; set; }
+
+        public TransaccionBL()
+        {
+            _contexto = new Contexto();
+            ListaTransaccion = new BindingList<Transaccion>();
+        }
+        public BindingList<Transaccion> ObtenerTransaccion()
+        {
+            _contexto.Transaccion.Load();
+            ListaTransaccion = _contexto.Transaccion.Local.ToBindingList();
+
+            return ListaTransaccion;
+        }
+
+
+
+
+
+    }
+    public class Transaccion
+
+    {
+        public int Id { get; set; }
+        public String status { get; set; }
+        public DateTime FechaTransaccion { get; set; }
+        public string UsuarioNombre { get; set; }
+        public int TipoUsuarioId { get; set; }
+        public int MedioId { get; set; }
+        public DateTime FechaRecibo { get; set; }
+        public DateTime FechaRespuesta { get; set; }
+        public string Receptor { get; set; }
+
+    }
+}
